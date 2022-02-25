bd7f1aaacb18c1ce01bf1e14f1cd1e284853d4f4
vuvkar <vuvkar@hotmail.co.uk>
2022-02-25 21:55:01 +0400

    resolution change with new fonts

--- desktop/src/com/phys/template/desktop/DesktopLauncher.java
+++ desktop/src/com/phys/template/desktop/DesktopLauncher.java
@@ -7,8 +7,8 @@ import com.phys.template.PhysTemplate;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 1600;
-		config.height = 900;
+		config.width = 1366;
+		config.height = 768;
 		new LwjglApplication(new PhysTemplate(), config);
 	}
 }
